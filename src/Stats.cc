d6a9f888a1814cafe1569f7e5f17cfab15a967f7
skowrons <jakub.skowronskI@pd.infn.it>
2025-02-06 09:19:14 +0100

    Increased time in which the stats are send to Graphite.

--- src/Stats.cc
+++ src/Stats.cc
@@ -114,7 +114,7 @@ void Stats::Thread( )
 	}
       }
     }
-    std::this_thread::sleep_for( std::chrono::milliseconds( 500 ) );
+    std::this_thread::sleep_for( std::chrono::milliseconds( 1000 ) );
   }
   if(sockCarbon!=-1){
     close(sockCarbon);
